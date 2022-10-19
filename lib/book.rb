d10dd72ef684ea482e8f6d7b86cbaf5c6228047c
david-commit <david.ondiege@students.moringaschool.com>
2022-10-19 17:49:42 +0300

    Complete assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,36 @@
+class Book
+ attr_accessor :author
+ attr_accessor :page_count
+ attr_accessor :genre
+ attr_accessor :turn_page
 
+ def initialize(title) #, author_name, page_count, genre
+  @title = title
+  # @author_name = author_name
+  # @page_count = page_count
+  # @genre = genre
+ end
+
+ def title
+  @title
+ end
+
+ def author
+  @author
+ end
+
+ def page_count
+  @page_count
+ end
+
+ def genre
+  @genre
+ end
+
+ def turn_page
+  @turn_page
+  puts "Flipping the page...wow, you read fast!"
+ end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,40 @@
 # Make your shoe class here!
+
+class Shoe
+ attr_accessor :color
+ attr_accessor :size
+ attr_accessor :color
+ attr_accessor :condition
+ attr_accessor :material
+
+ def initialize(brand)
+  @brand = brand
+ end
+
+ def brand
+  @brand
+ end
+
+ def color
+  @color
+ end
+
+ def size
+  @size
+ end
+
+ def material
+  @material
+ end
+
+ def condition
+  @condition
+ end
+
+ def cobble
+  @condition = "new"
+  puts "Your shoe is as good as new!"
+ end
+
+end
+
